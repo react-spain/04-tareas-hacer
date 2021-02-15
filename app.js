559655699bd84693fcce82841bcdff291e3a0cdd
Marlom <falconsoft.3d@gmail.com>
2021-02-15 17:15:50 +0100

    clase 46

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 require('colors');
 
+const { mostrarMenu, pausa } = require('./helpers/mensajes');
+
+console.clear();
+
 const main = () => {
     console.clear;
     console.log("Hola Mundo")
+    mostrarMenu();
+    // pausa();
 }
-
-
 
 main();
