50adca2fd555af6c946ecd7e9b049608a1325a94
Marlom <falconsoft.3d@gmail.com>
2021-02-15 17:33:41 +0100

    clase 46

--- app.js
+++ app.js
@@ -4,11 +4,14 @@ const { mostrarMenu, pausa } = require('./helpers/mensajes');
 
 console.clear();
 
-const main = () => {
-    console.clear;
+const main = async() => {
     console.log("Hola Mundo")
-    mostrarMenu();
-    // pausa();
-}
+    let opt = ''
+    do{
+        opt = await mostrarMenu();
+        console.log({ opt });
+        if ( opt !=='0' ) await pausa();
 
+    } while(opt !== '0' );
+}
 main();
